ca6095cedb4d9b5866b1b64d120aed7bf47b81cf
eyubz <eyerusalembezu15@gmail.com>
2024-10-30 07:54:25 +0300

    Files updated.

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" LRU caching """
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ LRU cache class """
+
+    def __init__(self):
+        """ constructor """
+        super().__init__()
+        self.keys = []
+
+    def put(self, key, item):
+        """ put an item to the cache """
+
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                val = self.keys.pop(0)
+                del self.cache_data[val]
+                print("DISCARD: {}".format(val))
+            if key in self.keys:
+                self.keys.remove(key)
+            self.cache_data[key] = item
+            self.keys.append(key)
+
+    def get(self, key):
+        """ get data from the cache """
+        if key is None or key not in self.cache_data:
+            return None
+        else:
+            return self.cache_data[key]
