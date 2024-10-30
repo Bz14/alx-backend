a5b918fbbfde06c7a8ce491c92c8ebe114935290
eyubz <eyerusalembezu15@gmail.com>
2024-10-30 08:46:58 +0300

    Files updated.

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """ LRU caching """
-from collections import OrderedDict
 
 
 BaseCaching = __import__('base_caching').BaseCaching
@@ -12,23 +11,24 @@ class LRUCache(BaseCaching):
     def __init__(self):
         """ constructor """
         super().__init__()
-        self.cache_data = OrderedDict()
+        self.keys = []
 
     def put(self, key, item):
         """ put an item to the cache """
+
         if key is not None and item is not None:
-            if key not in self.cache_data:
-                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                    val, _ = self.cache_data.popitem(True)
-                    print("DISCARD: {}".format(val))
-                self.cache_data[key] = item
-                self.cache_data.move_to_end(key, last=False)
-            else:
-                self.cache_data[key] = item
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                val = self.keys.pop(0)
+                del self.cache_data[val]
+                print("DISCARD: {}".format(val))
+            if key in self.keys:
+                self.keys.remove(key)
+            self.cache_data[key] = item
+            self.keys.append(key)
 
     def get(self, key):
         """ get data from the cache """
         if key is None or key not in self.cache_data:
-            self.cache_data.move_to_end(key, last=False)
-        return self.cache_data.get(key, None)
-    
+            return None
+        else:
+            return self.cache_data[key]
