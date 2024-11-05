af12c061e30820bfa243516f898b6f54dc8bd222
Bz14 <bezueyerusalem@gmail.com>
2024-11-05 21:59:01 +0300

    Files updated.

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,42 +4,25 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
+
 class Config:
     """Config class"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
+
 
 @app.route('/')
 def index() -> str:
     """index page"""
     return render_template('1-index.html')
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-# #!/usr/bin/env python3
-# """handling request to root URL /"""
-# from flask import Flask, render_template
-# from flask_babel import Babel
-
-
-# app = Flask(__name__)
-# app.config.from_pyfile('mysettings.cfg')
-# babel = Babel(app)
